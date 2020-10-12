6fb72ad22c8d00b097db68bbe5194a5eb2279bcd
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-10-12 23:28:50 +1100

    Formatted whitespace in all files
    - Tabs converted to 4 spaces
    - Any trailing whitespace stripped

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,11 +10,11 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
+        // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 }
